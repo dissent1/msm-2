7157043a5e978a70fa4d2575b6f520170a29bab7
Gowtham Siddarth Dhandapani <gdhandap@codeaurora.org>
2016-04-11 14:02:46 +0530

    ipq40xx: watchdog: Enabled additional prints for watchdog info
    Change-Id: I5662f414cd67dc1ad229a6482cf81397162ec927
    Signed-off-by: Gowtham Siddarth Dhandapani <gdhandap@codeaurora.org>
    Signed-off-by: Ajay Kishore <akisho@codeaurora.org>

--- drivers/watchdog/qcom-wdt.c
+++ drivers/watchdog/qcom-wdt.c
@@ -339,9 +339,11 @@ static irqreturn_t wdt_bark_isr(int irq, void *wdd)
 	writel(0, wdt_addr(wdt, WDT_EN));
 	writel(1, wdt_addr(wdt, WDT_BITE_TIME));
 	mb(); /* Avoid unpredictable behaviour in concurrent executions */
+	pr_info("Configuring Watchdog Timer\n");
 	writel(1, wdt_addr(wdt, WDT_RST));
 	writel(1, wdt_addr(wdt, WDT_EN));
 	mb(); /* Make sure the above sequence hits hardware before Reboot. */
+	pr_info("Waiting for Reboot\n");
 
 	mdelay(1);
 	pr_err("Wdog - CTL: 0x%x, BARK TIME: 0x%x, BITE TIME: 0x%x",
