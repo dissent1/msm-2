81b495b080e2cc9430411ad40261e53b9c9a32ef
Sujeev Dias <sdias@codeaurora.org>
2016-11-29 18:02:23 -0800

    mhi: core: add support for MHI to operate on slave mode
    MHI host is not always the bus master. Add support to
    operate on slave mode.
    
    Change-Id: I4af92c8171cf9ba38267afa753cec33f291d55eb
    Signed-off-by: Sujeev Dias <sdias@codeaurora.org>
    (cherry picked from commit 240727780fd6bd55dd95112987e03cbde4069a1c)
    Signed-off-by: Balamurugan Selvarajan <bselvara@codeaurora.org>

--- drivers/platform/msm/mhi/mhi_iface.c
+++ drivers/platform/msm/mhi/mhi_iface.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2014-2016, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2014-2017, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -204,7 +204,7 @@ static int mhi_pci_probe(struct pci_dev *pcie_device,
 	tasklet_init(&mhi_dev_ctxt->ev_task,
 		     mhi_ctrl_ev_task,
 		     (unsigned long)mhi_dev_ctxt);
-
+	init_completion(&mhi_dev_ctxt->cmd_complete);
 	mhi_dev_ctxt->flags.link_up = 1;
 
 #ifdef CONFIG_MSM_BUS_SCALING
@@ -292,8 +292,8 @@ static int mhi_pci_probe(struct pci_dev *pcie_device,
 	/* setup shadow pm functions */
 	mhi_dev_ctxt->assert_wake = mhi_assert_device_wake;
 	mhi_dev_ctxt->deassert_wake = mhi_deassert_device_wake;
-	mhi_dev_ctxt->runtime_get = mhi_runtime_get;
-	mhi_dev_ctxt->runtime_put = mhi_runtime_put;
+	mhi_dev_ctxt->runtime_get = mhi_master_mode_runtime_get;
+	mhi_dev_ctxt->runtime_put = mhi_master_mode_runtime_put;
 
 	mutex_lock(&mhi_dev_ctxt->pm_lock);
 	write_lock_irq(&mhi_dev_ctxt->pm_xfer_lock);
