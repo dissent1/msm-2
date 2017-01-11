caf-backup/quic_la_kernel_msm

qcom: ipq806x: Check bad block before reading bootconfig partition
The bootconfig partitions has 2 blocks for storing the partition
information. If one block goes bad , then other block can store
the paritions information. So checking the bad blocks before reading
the bootconfig paritions.

Change-Id: Ic8196001699fc4c0a65a4a4fd194376345731c4f
Signed-off-by: Sham Muthayyan <smuthayy@codeaurora.org>

drivers/platform/msm/bootconfig.c
@@ -167,6 +167,14 @@ struct sbl_if_dualboot_info_type_v2 *read_bootconfig_mtd(
 	struct sbl_if_dualboot_info_type_v2 *bootconfig_mtd;
 	int ret;
 
+	while (mtd_block_isbad(master, offset)) {
+		offset += master->erasesize;
+		if (offset >= master->size) {
+			pr_alert("Bad blocks occured while reading from \"%s\"\n",
+					master->name);
+			return NULL;
+		}
+	}
 	bootconfig_mtd = kmalloc(sizeof(struct sbl_if_dualboot_info_type_v2),
 				   GFP_ATOMIC);
 

dissent1/msm-2

(cherry picked from commit 0b6def72bc7e7f7518304b197b06872a512c088f)

drivers/platform/ipq/bootconfig.c
@@ -167,6 +167,14 @@ struct sbl_if_dualboot_info_type_v2 *read_bootconfig_mtd(
 	struct sbl_if_dualboot_info_type_v2 *bootconfig_mtd;
 	int ret;
 
+	while (mtd_block_isbad(master, offset)) {
+		offset += master->erasesize;
+		if (offset >= master->size) {
+			pr_alert("Bad blocks occurred while reading from \"%s\"\n",
+					master->name);
+			return NULL;
+		}
+	}
 	bootconfig_mtd = kmalloc(sizeof(struct sbl_if_dualboot_info_type_v2),
 				   GFP_ATOMIC);
 