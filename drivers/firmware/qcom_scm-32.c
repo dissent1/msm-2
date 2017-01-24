2034addc7e193dc81d7ca60d8884832751b76758
Ajay Kishore <akisho@codeaurora.org>
2017-01-24 14:14:16 +0530

    pinctrl: qcom: use scm_call to route GPIO irq to Apps
    For IPQ806x targets, TZ protects the registers that are used to
    configure the routing of interrupts to a target processor.
    To resolve this, this patch uses scm call to route GPIO interrupts
    to application processor. Also the scm call interface is changed.
    
    Change-Id: Ib6c06829d04bc8c20483c36e63da92e26cdef9ce
    Signed-off-by: Ajay Kishore <akisho@codeaurora.org>

--- drivers/firmware/qcom_scm-32.c
+++ drivers/firmware/qcom_scm-32.c
@@ -615,6 +615,23 @@ int __qcom_scm_tzsched(struct device *dev,
 
 	ret = qcom_scm_call(dev, svc_id, cmd_id, req,
 				req_size, resp, resp_size);
+	return ret;
+}
+
+int __qcom_scm_pinmux_read(u32 svc_id, u32 cmd_id, u32 arg1)
+{
+	s32 ret;
+
+	ret = qcom_scm_call_atomic1(svc_id, cmd_id, arg1);
+
+	return ret;
+}
+
+int __qcom_scm_pinmux_write(u32 svc_id, u32 cmd_id, u32 arg1, u32 arg2)
+{
+	s32 ret;
+
+	ret = qcom_scm_call_atomic2(svc_id, cmd_id, arg1, arg2);
 
 	return ret;
 }
