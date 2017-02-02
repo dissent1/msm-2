515e0a7dbf648f56f074dc3a5997a56ada2f1a68
Jaiganesh Narayanan <njaigane@codeaurora.org>
2017-02-02 11:16:00 +0530

    ipq: add scm wrapper for qseecom driver
    Change-Id: I70480d4c97981933d463d8bb8332a9bf083e0242
    Signed-off-by: Jaiganesh Narayanan <njaigane@codeaurora.org>

--- drivers/firmware/qcom_scm-64.c
+++ drivers/firmware/qcom_scm-64.c
@@ -338,3 +338,9 @@ int __qcom_scm_sdi(u32 svc_id, u32 cmd_id)
 {
 	return -ENOTSUPP;
 }
+
+int __qcom_scm_tzsched(u32 svc_id, u32 cmd_id, const void *req,
+			size_t req_size, void *resp, size_t resp_size)
+{
+	return -ENOTSUPP;
+}
