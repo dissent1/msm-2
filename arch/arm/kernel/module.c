175e248818ec173bdd4bcbba5d9bae58c73ab3d9
Jaiganesh Narayanan <njaigane@codeaurora.org>
2017-02-09 16:42:29 +0530

    arm: ipq: supress module vmalloc failure messages
    when CONFIG_ARM_MODULE_PLTS is enabled, existing kernel module alloc
    code tries to allocate memory for the module from module region.
    If module region memory alloc fails then the vmalloc region is
    chosen for the memory allocation. During this flow, if the module
    region memory alloc fails then vmalloc failure message is printed
    but the memory alloc succeeds from the vmalloc region. This misleading
    vmalloc failure message is unnecessary and creates confusion.
    
    This fix addresses the above explained issue by suppressing the vmalloc
    failure message from module region memory allocation if
    CONFIG_ARM_MODULE_PLTS kernel config is enabled.
    
    Change-Id: I3f715b924d54e5ed076f5ea731909bdd9875d86c
    Signed-off-by: Jaiganesh Narayanan <njaigane@codeaurora.org>

--- arch/arm/kernel/module.c
+++ arch/arm/kernel/module.c
@@ -40,11 +40,19 @@
 #ifdef CONFIG_MMU
 void *module_alloc(unsigned long size)
 {
-	void *p = __vmalloc_node_range(size, 1, MODULES_VADDR, MODULES_END,
-				GFP_KERNEL, PAGE_KERNEL_EXEC, 0, NUMA_NO_NODE,
+	unsigned long gfp_flags = GFP_KERNEL;
+	void *p;
+
+	if (IS_ENABLED(CONFIG_ARM_MODULE_PLTS))
+		gfp_flags = gfp_flags | __GFP_NOWARN;
+
+	p = __vmalloc_node_range(size, 1, MODULES_VADDR, MODULES_END,
+				gfp_flags, PAGE_KERNEL_EXEC, 0, NUMA_NO_NODE,
 				__builtin_return_address(0));
+
 	if (!IS_ENABLED(CONFIG_ARM_MODULE_PLTS) || p)
 		return p;
+
 	return __vmalloc_node_range(size, 1,  VMALLOC_START, VMALLOC_END,
 				GFP_KERNEL, PAGE_KERNEL_EXEC, 0, NUMA_NO_NODE,
 				__builtin_return_address(0));
